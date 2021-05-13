c03a01a8181128d5803212876115192f83508d6a
임승현 <mmomm.676@gamil.com>
2021-05-13 10:59:28 +0900

    수정4

---
+++ pybo/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    # sqlalchmy db 설정
+    app.config.from_object(config)
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    from . import models
+
+    from pybo.views import auth_views
+    from pybo.views import user_views
+    from pybo.views import naver_views
+    from pybo.views import main_views
+    from pybo.views import question_views
+    from pybo.views import answer_views
+    from pybo.views import movie_views
+    from .filter import format_datetime
+
+    app.jinja_env.filters['datetime']=format_datetime
+
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(naver_views.bp)
+    app.register_blueprint(user_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
+    app.register_blueprint(movie_views.bp)
+    return app
---
+++ pybo/naverapi.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import urllib.request
+import json
+
+def naverbook(bookname):
+
+    client_id = "JZTnikyvzsfKdTQuMfe3"
+    client_secret = "8ZzzPSDLHB"
+
+    encText = urllib.parse.quote(bookname)
+    url = "https://openapi.naver.com/v1/search/book?query=" + encText # json 결과
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        jsontemp=json.loads(response_body.decode('utf-8'))
+
+    else:
+        print("Error Code:" + rescode)
+
+
+    return jsontemp
+
+def navermovie(moviename):
+
+    client_id = "JZTnikyvzsfKdTQuMfe3"
+    client_secret = "8ZzzPSDLHB"
+
+    encText = urllib.parse.quote(moviename)
+    url = "https://openapi.naver.com/v1/search/movie?query=" + encText # json 결과
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        jsontemp=json.loads(response_body.decode('utf-8'))
+
+    else:
+        print("Error Code:" + rescode)
+
+
+    return jsontemp
+
+print(navermovie("스파이럴"))
