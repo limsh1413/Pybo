768942eef8aeb2965d75000e8c06669f3be9e022
임승현 <mmomm.676@gamil.com>
2021-05-14 14:54:52 +0900

    pybo.py추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -7,32 +7,31 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app():
-    app = Flask(__name__)
 
-    # sqlalchmy db 설정
-    app.config.from_object(config)
-    db.init_app(app)
-    migrate.init_app(app,db)
+app = Flask(__name__)
 
-    from . import models
+# sqlalchmy db 설정
+app.config.from_object(config)
+db.init_app(app)
+migrate.init_app(app,db)
 
-    from pybo.views import auth_views
-    from pybo.views import user_views
-    from pybo.views import naver_views
-    from pybo.views import main_views
-    from pybo.views import question_views
-    from pybo.views import answer_views
-    from pybo.views import movie_views
-    from .filter import format_datetime
+from . import models
 
-    app.jinja_env.filters['datetime']=format_datetime
+from pybo.views import auth_views
+from pybo.views import user_views
+from pybo.views import naver_views
+from pybo.views import main_views
+from pybo.views import question_views
+from pybo.views import answer_views
+from pybo.views import movie_views
+from .filter import format_datetime
 
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(naver_views.bp)
-    app.register_blueprint(user_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(movie_views.bp)
-    return app
+app.jinja_env.filters['datetime']=format_datetime
+
+app.register_blueprint(main_views.bp)
+app.register_blueprint(naver_views.bp)
+app.register_blueprint(user_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(movie_views.bp)
